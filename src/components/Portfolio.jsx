2f364253865751a3db9ec3c1ffa562b720959bca
= <80379977+rayan2228@users.noreply.github.com>
2023-09-14 15:45:37 +0600

    portfolio responsive

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,16 +9,16 @@ import project2 from "../assets/project(2).png";
 import project3 from "../assets/project(3).png";
 const Portfolio = () => {
   return (
-    <section className="mt-[208px]">
+    <section className="mt-[208px] px-2">
       <Container>
-        <Flex className={"items-center justify-between"}>
-          <Title smallTitle={"Recent Projects"} title={"My Portfolio"} />
+        <Flex className={"lg:items-center justify-between lg:flex-row flex-col "}>
+          <Title smallTitle={"Recent Projects"} title={"My Portfolio"}className={"text-5xl my-5"} />
           <a href="" className="bg-[#E62872] py-3 px-5 rounded-xl text-white">
             <BsDribbble className="inline-block mr-4 font-rob text-xl font-normal"/>
             Visit My Dribbble
           </a>
         </Flex>
-        <Flex className={"justify-between mt-[106px]"}>
+        <Flex className={"justify-between mt-[106px] flex-col lg:flex-row gap-y-4 lg:gap-y-0"}>
             <Project projectDetails={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros."} projectName={"Ahuse"} projectImage={project1}/>
             <Project projectDetails={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros."} projectName={"App Dashboard"} projectImage={project2}/>
             <Project projectDetails={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros."} projectName={"Easy Rent"} projectImage={project3}/>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,7 +3,7 @@ import Image from "./layouts/Image";
 import { FiArrowUpRight } from "react-icons/fi";
 const Project = ({ projectImage, projectName, projectDetails }) => {
   return (
-    <div className="w-[32%] font-rob rounded-lg shadow-project-shadow">
+    <div className="w-full lg:w-[32%] font-rob rounded-lg shadow-project-shadow">
       <Image src={projectImage} imgClassName={"w-full"} />
       <div className="p-8">
         <h2 className=" text-[32px] text-text-primary font-bold">
